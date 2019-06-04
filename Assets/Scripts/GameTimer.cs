550ed28b53b29dce5039e21e3a6af1bcc1de494b
Mário Gil <mg_mesquita@hotmail.com>
2019-06-04 01:26:51 +0100

    Game timer on top right corner; Fixed player z index;

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+
+    private TimeCounter timeCounter;
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter = GameObject.FindObjectOfType<TimeCounter>();
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int seconds = timeCounter.getTime();
+
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+
+        string button_text = "";
+        if (minutes < 10) button_text += "0";
+        button_text += minutes + " : ";
+        if (seconds < 10) button_text += "0";
+        button_text += seconds;
+
+        text.text = button_text;
+    }
+}
